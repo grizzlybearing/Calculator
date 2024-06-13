67108bbeb0e317a32210275c1c4ddba593680278
Selectel Git User <git-user@selectel.ru>
2024-06-13 17:48:05 +0300

    MainWindow

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputExpression = ExpressionInput.Text;
+
+            try
+            {
+
+                //ResultOutput.Text = EvaluateExpression(inputExpression).ToString();
+            }
+            catch (Exception ex)
+            {
+                ResultOutput.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExpressionInput.Text = string.Empty;
+            ResultOutput.Text = string.Empty;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+    }
+}
