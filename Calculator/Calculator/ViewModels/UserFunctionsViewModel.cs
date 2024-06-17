f9bc5da492fe85747536e288ae116e973b81015e
dartderden <158480745+dartderden@users.noreply.github.com>
2024-06-17 03:18:13 +0300

    Add files via upload

---
+++ Calculator/Calculator/ViewModels/UserFunctionsViewModel.cs
@@ -0,0 +1,60 @@
+﻿
+using Prism.Mvvm;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp2.Model;
+
+namespace WpfApp2.ViewModel
+{
+    internal class UserFunctionsViewModel: BindableBase
+    
+        {
+            public ObservableCollection<UserFunction> Functions { get; set; }
+
+            private string _newFunction;
+            public string NewFunction
+            {
+                get =>_newFunction;
+                set => SetProperty(ref _newFunction, value);
+            }
+
+            public ICommand AddFunctionCommand { get; set; }
+            public ICommand DeleteFunctionCommand { get; set; }
+
+            public UserFunctionsViewModel()
+            {
+                Functions = new ObservableCollection<UserFunction>();
+
+                AddFunctionCommand = new DelegateCommand(AddFunction);
+                DeleteFunctionCommand = new DelegateCommand<UserFunction>(DeleteFunction);
+            }
+
+        private void AddFunction()
+        {
+            if (!string.IsNullOrWhiteSpace(NewFunction))
+            {
+                Functions.Add(new UserFunction { Function = NewFunction});
+                NewFunction = string.Empty;
+            }
+        }
+
+        private void DeleteFunction(UserFunction function)
+        {
+            if (function != null)
+            {
+                Functions.Remove(function);
+            }
+        }
+    }
+    
+    
+ }
+
